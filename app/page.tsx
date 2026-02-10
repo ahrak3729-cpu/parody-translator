009136312240d9c15de0e3792e08b3fe87c1d3c8
ahrak3729-cpu <ahrak3729@gmail.com>
2026-02-10 20:25:42 +0900

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Parody Translator</h1>
+      <p>번역기 초기 화면</p>
+    </main>
+  );
+}
